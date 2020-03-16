4d4528c5b18c8aa930b3715a295afbc3dfa8ac90
Ihor Bahlay <ihbaglie@gmail.com>
2020-03-16 22:42:52 +0300

    new system getting shells, reworked now

--- src/main/java/automatic/fire/complex/ShellsSystem/Cassette.java
+++ src/main/java/automatic/fire/complex/ShellsSystem/Cassette.java
@@ -1,21 +1,23 @@
 package automatic.fire.complex.ShellsSystem;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Cassette<T extends Shell> {
     private List<T> shells;
+    private int startQuantity;
     private int balance;
 
-    public Cassette(){}
-    public Cassette(int balance) {
-        this.balance = balance;
-        shells = new ArrayList<>(balance);
+    public Cassette(int startQuantity) {
+        this.startQuantity = startQuantity;
+        shells = new ArrayList<>(startQuantity);
     }
 
     public boolean add(T shell) {
-        if (shells.size() <= balance) {
+        if (shells.size() < startQuantity) {
             shells.add(shell);
+            balance = shells.size();
             return true;
         }else {
             return false;
@@ -31,14 +33,15 @@ public class Cassette<T extends Shell> {
     }
 
     public T getShell() {
-        return shells.get(balance);
+        shells.remove(shells.size()-1);
+        balance = shells.size();
+        return shells.get(0);
     }
 
     public int getBalance() {
         return balance;
     }
-
-    public void setBalance(int balance) {
-        this.balance = balance;
+    public T getInstanceInnerElement() {
+        return shells.get(0);
     }
 }
