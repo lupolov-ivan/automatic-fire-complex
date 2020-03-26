2504f95ddeb2d0add1d297667af606faa77bd977
Ivan Lupolov <iv.lupolov@gmail.com>
2020-03-26 17:02:03 +0200

    deleted unused log from Tank and Infantry

--- src/main/java/automatic/fire/complex/units/enemy/Infantry.java
+++ src/main/java/automatic/fire/complex/units/enemy/Infantry.java
@@ -1,12 +1,8 @@
 package automatic.fire.complex.units.enemy;
 
 import automatic.fire.complex.simulation.RealitySimulationModule;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class Infantry extends Enemy implements Runnable {
-
-    Logger log = LoggerFactory.getLogger(Infantry.class);
 
     public Infantry(int posX, int posY, int protectionLevel, int speed, RealitySimulationModule rsm) {
         super(posX, posY, protectionLevel, speed, rsm);
--- src/main/java/automatic/fire/complex/units/enemy/Tank.java
+++ src/main/java/automatic/fire/complex/units/enemy/Tank.java
@@ -1,12 +1,8 @@
 package automatic.fire.complex.units.enemy;
 
 import automatic.fire.complex.simulation.RealitySimulationModule;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class Tank extends Enemy implements Runnable {
-
-    Logger log = LoggerFactory.getLogger(Tank.class);
 
     public Tank(int posX, int posY, int protectionLevel, int speed, RealitySimulationModule rsm) {
         super(posX, posY, protectionLevel, speed, rsm);
